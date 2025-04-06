1b60e77a304ea23eea318e4469cb0c272fb3de6a
吉嶺勇斗 <jilingyongdou@gmail.com>
2025-04-06 20:55:38 +0900

    gitinore追加

---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+            refresh: true,
+        }),
+        react(),
+    ],
+    server: {
+        port: 5177,
+        cors: true,
+        hmr:{
+            host: 'localhost',
+        },
+    },
+});
